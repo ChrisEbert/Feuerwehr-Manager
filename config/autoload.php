f5f20573d6ab383767dcb6ddf63e6a1eddcace5f
ChrisEbert <chris_ebert@gmx.de>
2017-04-23 16:31:56 +0200

    Fix wrong autoloader paths

--- config/autoload.php
+++ config/autoload.php
@@ -14,32 +14,31 @@
  */
 ClassLoader::addClasses(array
 (
-	'AlertDetails'                => 'system/modules/feuerwehr_manager/AlertDetails.php',
-	'BeModuleAlertStatisticTotal' => 'system/modules/feuerwehr_manager/BeModuleAlertStatisticTotal.php',
-	'BeModuleAlertStatisticYear'  => 'system/modules/feuerwehr_manager/BeModuleAlertStatisticYear.php',
+	'AlertDetails'                => 'system/modules/feuerwehr-manager/AlertDetails.php',
+	'BeModuleAlertStatisticTotal' => 'system/modules/feuerwehr-manager/BeModuleAlertStatisticTotal.php',
+	'BeModuleAlertStatisticYear'  => 'system/modules/feuerwehr-manager/BeModuleAlertStatisticYear.php',
 	// Classes
-	'Contao\Alert'                => 'system/modules/feuerwehr_manager/classes/Alert.php',
-	'Contao\AlertFbBot'                  => 'system/modules/feuerwehr_manager/classes/AlertFbBot.php',
-	'ContentAlertStatistic'       => 'system/modules/feuerwehr_manager/ContentAlertStatistic.php',
-	'DataContainerStatistic'      => 'system/modules/feuerwehr_manager/DataContainerStatistic.php',
-	'generateCharts'              => 'system/modules/feuerwehr_manager/generateCharts.php',
+	'Contao\Alert'                => 'system/modules/feuerwehr-manager/classes/Alert.php',
+	'ContentAlertStatistic'       => 'system/modules/feuerwehr-manager/ContentAlertStatistic.php',
+	'DataContainerStatistic'      => 'system/modules/feuerwehr-manager/DataContainerStatistic.php',
+	'generateCharts'              => 'system/modules/feuerwehr-manager/generateCharts.php',
 
 	// Models
-	'Contao\FwmAlertsModel'       => 'system/modules/feuerwehr_manager/models/FwmAlertsModel.php',
-	'Contao\FwmDepartmentsModel'  => 'system/modules/feuerwehr_manager/models/FwmDepartmentsModel.php',
-	'Contao\FwmVehiclesModel'     => 'system/modules/feuerwehr_manager/models/FwmVehiclesModel.php',
-	'ModuleAlertMap'              => 'system/modules/feuerwehr_manager/ModuleAlertMap.php',
-	'ModuleAlertStatisticReader'  => 'system/modules/feuerwehr_manager/ModuleAlertStatisticReader.php',
-	'ModuleAlertStatisticTotal'   => 'system/modules/feuerwehr_manager/ModuleAlertStatisticTotal.php',
-	'ModuleAlertStatisticYear'    => 'system/modules/feuerwehr_manager/ModuleAlertStatisticYear.php',
-	'ModuleAlertStatisticYears'   => 'system/modules/feuerwehr_manager/ModuleAlertStatisticYears.php',
+	'Contao\FwmAlertsModel'       => 'system/modules/feuerwehr-manager/models/FwmAlertsModel.php',
+	'Contao\FwmDepartmentsModel'  => 'system/modules/feuerwehr-manager/models/FwmDepartmentsModel.php',
+	'Contao\FwmVehiclesModel'     => 'system/modules/feuerwehr-manager/models/FwmVehiclesModel.php',
+	'ModuleAlertMap'              => 'system/modules/feuerwehr-manager/ModuleAlertMap.php',
+	'ModuleAlertStatisticReader'  => 'system/modules/feuerwehr-manager/ModuleAlertStatisticReader.php',
+	'ModuleAlertStatisticTotal'   => 'system/modules/feuerwehr-manager/ModuleAlertStatisticTotal.php',
+	'ModuleAlertStatisticYear'    => 'system/modules/feuerwehr-manager/ModuleAlertStatisticYear.php',
+	'ModuleAlertStatisticYears'   => 'system/modules/feuerwehr-manager/ModuleAlertStatisticYears.php',
 
 	// Modules
-	'Contao\ModuleAlertList'      => 'system/modules/feuerwehr_manager/modules/ModuleAlertList.php',
-	'Contao\ModuleLastAlert'      => 'system/modules/feuerwehr_manager/modules/ModuleLastAlert.php',
+	'Contao\ModuleAlertList'      => 'system/modules/feuerwehr-manager/modules/ModuleAlertList.php',
+	'Contao\ModuleLastAlert'      => 'system/modules/feuerwehr-manager/modules/ModuleLastAlert.php',
 
 	// Widgets
-	'Contao\SearchMap'            => 'system/modules/feuerwehr_manager/widgets/SearchMap.php',
+	'Contao\SearchMap'            => 'system/modules/feuerwehr-manager/widgets/SearchMap.php',
 ));
 
 
@@ -48,14 +47,14 @@ ClassLoader::addClasses(array
  */
 TemplateLoader::addFiles(array
 (
-	'be_alert_statistic_total' => 'system/modules/feuerwehr_manager/templates',
-	'be_alert_statistic_year'  => 'system/modules/feuerwehr_manager/templates',
-	'be_map_widget'            => 'system/modules/feuerwehr_manager/templates',
-	'ce_alert_statistic'       => 'system/modules/feuerwehr_manager/templates',
-	'fwm_alertDetailsMap'      => 'system/modules/feuerwehr_manager/templates',
-	'fwm_alertMap'             => 'system/modules/feuerwehr_manager/templates',
-	'fwm_alertStatisticReader' => 'system/modules/feuerwehr_manager/templates',
-	'fwm_alertStatisticYears'  => 'system/modules/feuerwehr_manager/templates',
-	'mod_fwm_alert_list'       => 'system/modules/feuerwehr_manager/templates',
-	'mod_fwm_last_alert'       => 'system/modules/feuerwehr_manager/templates',
+	'be_alert_statistic_total' => 'system/modules/feuerwehr-manager/templates',
+	'be_alert_statistic_year'  => 'system/modules/feuerwehr-manager/templates',
+	'be_map_widget'            => 'system/modules/feuerwehr-manager/templates',
+	'ce_alert_statistic'       => 'system/modules/feuerwehr-manager/templates',
+	'fwm_alertDetailsMap'      => 'system/modules/feuerwehr-manager/templates',
+	'fwm_alertMap'             => 'system/modules/feuerwehr-manager/templates',
+	'fwm_alertStatisticReader' => 'system/modules/feuerwehr-manager/templates',
+	'fwm_alertStatisticYears'  => 'system/modules/feuerwehr-manager/templates',
+	'mod_fwm_alert_list'       => 'system/modules/feuerwehr-manager/templates',
+	'mod_fwm_last_alert'       => 'system/modules/feuerwehr-manager/templates',
 ));
